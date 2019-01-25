988bd377bafc0f4dd331d9397dc3f045fb96de3d
marius.tudor <marius.tudor@verizon.net>
2019-01-25 12:24:10 -0500

    Updated one email address  for users.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', function(request, response) {
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bobnew@gmail.com"},
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob_pay_atention_this_is_from_heroku@gmail.com"},
     {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
     {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
