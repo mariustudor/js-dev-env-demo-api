12d4224661563244aab855291c6a7b2d77c9ad8d
marius.tudor <marius.tudor@verizon.net>
2019-01-25 11:14:42 -0500

    Updated Readme. Updated github path in app.json. Updated '/users' handle. Added eslint, @types/cors and .express. Updated github in package.jason. Added a few words to folder dictionary.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express =  require('express');
 var cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors()); // enable cross origin resource sharing.
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,7 +13,7 @@ app.get('/', function(request, response) {
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob_new@gmail.com"},
     {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
     {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
